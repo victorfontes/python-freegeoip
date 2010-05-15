948b40d666c00ab858d406c89f10e6eba991fbbe
Victor Fontes Costa <victor@macbook-de-victor-fontes-costa.local>
2010-05-15 01:56:54 -0300

    first commit

---
+++ freegeoip.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Searches Geolocation of IP addresses using http://freegeoip.net/
+It will fetch a csv and return a python dictionary
+
+sample usage:
+>>> from freegeoip import get_geodata
+>>> get_geodata("189.24.179.76")
+
+{'status': True, 'city': 'Niter\xc3\xb3i', 'countrycode': 'BR', 'ip': '189.24.179.76', 
+'zipcode': '', 'longitude': '-43.0944', 'countryname': 'Brazil', 'regioncode': '21', 
+'latitude': '-22.8844', 'regionname': 'Rio de Janeiro'}
+"""
+
+from urllib import urlopen
+from csv import reader
+import sys
+import re
+
+__author__="Victor Fontes Costa"
+__copyright__ = "Copyright (c) 2010, Victor Fontes - victorfontes.com"
+__license__ = "GPL"
+__version__ = "2.1"
+__maintainer__ = __author__
+__email__ = "contato [a] victorfontes.com"
+__status__ = "Development"
+
+FREE_GEOIP_CSV_URL = "http://freegeoip.net/csv/%s"
+
+
+def valid_ip(ip):
+
+    pattern = r"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+
+    return re.match(pattern, ip)
+
+def _get_geodata_csv(ip):
+    if not valid_ip(ip):
+        raise Exception('Invalid IP format', 'You must enter a valid ip format: X.X.X.X')
+    
+    URL = FREE_GEOIP_CSV_URL % ip
+    response_csv = reader(urlopen(URL))
+    csv_data = response_csv.next()
+
+    return {
+        "status": u"True" == csv_data[0],
+        "ip":csv_data[1],
+        "countrycode":csv_data[2],
+        "countryname":csv_data[3],
+        "regioncode":csv_data[4],
+        "regionname":csv_data[5],
+        "city":csv_data[6],
+        "zipcode":csv_data[7],
+        "latitude":csv_data[8],
+        "longitude":csv_data[9]
+    }
+
+def get_geodata(ip):
+    return _get_geodata_csv(ip)
+
+if __name__ == "__main__":     #code to execute if called from command-line
+    ip = sys.argv[1]
+    print get_geodata(ip)
